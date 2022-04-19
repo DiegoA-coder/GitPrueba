603e07ca75022aa49bc9f6b3a12f6bf0efd905dc
DiegoA-coder <ogeid12@outlook.es>
2022-04-19 11:24:33 -0500

    Inyectando error

--- Prueba/Hello.java
+++ Prueba/Hello.java
@@ -1,7 +1,8 @@
 package Prueba;
 public class Hello {
   public static void main(String[] args) {
-    System.out.println("Hola mundo3");
+    System.out.println("Hola mundo");
+    System.out.println("Inyectando un error");
     System.out.println("Hola mundo2");
     System.out.println("Hola mundo1x");
   }
