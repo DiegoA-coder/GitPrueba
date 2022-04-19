0a82b62c2d0e5c825187c5e4440ecd3bcab9fcfe
DiegoA-coder <ogeid12@outlook.es>
2022-04-19 12:06:38 -0500

    actualizacion

--- Prueba/Hello.java
+++ Prueba/Hello.java
@@ -7,7 +7,7 @@ public class Hello {
     System.out.println("Hola mundo2");
     System.out.println("Hola mundo1x");
 
-
+    System.out.println("Inyectando un error");
     System.out.println("Branch 2");
     System.out.println("Branch 2.0");
   }
