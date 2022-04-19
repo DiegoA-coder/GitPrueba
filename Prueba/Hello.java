4f95bf4a2bae650343faa8362f64eedec0bcc017
DiegoA-coder <ogeid12@outlook.es>
2022-04-19 12:11:03 -0500

    Hola mundo
    Hola mundo nuevo
    
    Hola mundo 2
    
    Hola mundo 3
    
    Hola Mundo actualizacion
    
    Nuevas caracteristicas (#1)
    
    * Nueva Rama
    
    * Nueva rama 2
    
    erros
    
    actualizacion
    
    caract (#2)
    
    * caract
    
    * mas+
    
    * mas
    
    * Squashed commit of the following:
    
    commit 0a82b62c2d0e5c825187c5e4440ecd3bcab9fcfe
    Author: DiegoA-coder <ogeid12@outlook.es>
    Date:   Tue Apr 19 12:06:38 2022 -0500
    
        actualizacion
    
    commit bad2dffab024f37b4b9a4ab67f3d40f0f5a3e23c
    Author: DiegoA-coder <ogeid12@outlook.es>
    Date:   Tue Apr 19 12:05:55 2022 -0500
    
        erros
    
    Inyectando error

---
+++ Prueba/Hello.java
@@ -0,0 +1,15 @@
+package Prueba;
+public class Hello {
+  public static void main(String[] args) {
+    System.out.println("Hola mundo");
+    System.out.println("Inyectando un error");
+    System.out.println("Inyectando un error x2");
+    System.out.println("Hola mundo2");
+    System.out.println("Hola mundo1x");
+
+    System.out.println("Inyectando un error");
+    System.out.println("Branch 2");
+    System.out.println("Branch 2.0");
+    System.out.println("Metiendo caracteristicas");
+  }
+}
