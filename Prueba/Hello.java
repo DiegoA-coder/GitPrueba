bad2dffab024f37b4b9a4ab67f3d40f0f5a3e23c
DiegoA-coder <ogeid12@outlook.es>
2022-04-19 12:05:55 -0500

    erros

--- Prueba/Hello.java
+++ Prueba/Hello.java
@@ -3,6 +3,7 @@ public class Hello {
   public static void main(String[] args) {
     System.out.println("Hola mundo");
     System.out.println("Inyectando un error");
+    System.out.println("Inyectando un error x2");
     System.out.println("Hola mundo2");
     System.out.println("Hola mundo1x");
 
