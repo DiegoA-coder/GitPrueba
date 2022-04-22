e29b48f4fe15efefc91f4a2f886d77d556b5af35
DiegoA-coder <ogeid12@outlook.es>
2022-04-22 10:15:35 -0500

    cambio
    Nueva Rama
    
    Nueva rama 2
    
    Inyectando error
    
    agregar cosas
    
    agregar

--- Prueba/Hello.java
+++ Prueba/Hello.java
@@ -3,13 +3,12 @@ public class Hello {
   public static void main(String[] args) {
     System.out.println("Hola mundo");
     System.out.println("Inyectando un error");
+<<<<<<< HEAD
     System.out.println("Inyectando un error x2");
+=======
+>>>>>>> 603e07c... Inyectando error
     System.out.println("Hola mundo2");
-    System.out.println("Hola mundo1x");
-
-    System.out.println("Inyectando un error");
-    System.out.println("Branch 2");
+    System.out.println("coasssss");
     System.out.println("Branch 2.0");
-    System.out.println("Metiendo caracteristicas");
   }
 }
